d405728d13f6fae445f32c3d202df93ae371dd4f
eaa <enesalp_aslan@outlook.com>
2024-11-08 02:02:46 +0300

    Fix Docker configuration for production

--- src/main/java/pt/attendancetracking/security/OpenApiConfig.java
+++ src/main/java/pt/attendancetracking/security/OpenApiConfig.java
@@ -1,33 +1,33 @@
-package pt.attendancetracking.security;
-
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-
-@Configuration
-public class OpenApiConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        final String securitySchemeName = "basicAuth";
-        return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement()
-                        .addList(securitySchemeName))
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                                new SecurityScheme()
-                                        .name(securitySchemeName)
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("basic")))
-                .info(new Info()
-                        .title("Attendance Tracking API")
-                        .version("1.0")
-                        .description("API for gym attendance tracking system"));
-    }
-
-}
+//package pt.attendancetracking.security;
+//
+//import io.swagger.v3.oas.models.Components;
+//import io.swagger.v3.oas.models.OpenAPI;
+//import io.swagger.v3.oas.models.info.Info;
+//import io.swagger.v3.oas.models.security.SecurityRequirement;
+//import io.swagger.v3.oas.models.security.SecurityScheme;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//
+//@Configuration
+//public class OpenApiConfig {
+//
+//    @Bean
+//    public OpenAPI customOpenAPI() {
+//        final String securitySchemeName = "basicAuth";
+//        return new OpenAPI()
+//                .addSecurityItem(new SecurityRequirement()
+//                        .addList(securitySchemeName))
+//                .components(new Components()
+//                        .addSecuritySchemes(securitySchemeName,
+//                                new SecurityScheme()
+//                                        .name(securitySchemeName)
+//                                        .type(SecurityScheme.Type.HTTP)
+//                                        .scheme("basic")))
+//                .info(new Info()
+//                        .title("Attendance Tracking API")
+//                        .version("1.0")
+//                        .description("API for gym attendance tracking system"));
+//    }
+//
+//}
